ac3f9070af01a6c32de4d27924f10fb469cc88e8
svenWacker <s.u.wacker@outlook.de>
2021-06-21 15:38:53 +0200

    framing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <React.Fragment>
-      <h1 className="main-heading">Hiewr findet Ihr uns</h1>
+      <h1 className="main-heading">Hier findet Ihr uns</h1>
       <div className="contact">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5176.656141396443!2d8.0677793!3d49.5538327!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x682f70d2122924b0!2sCasa%20Verde!5e0!3m2!1sde!2sde!4v1614000047483!5m2!1sde!2sde"
