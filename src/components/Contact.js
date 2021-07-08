7a1a55cce545ccf353906e7c6bc4280a3321781f
svenWacker <s.u.wacker@outlook.de>
2021-07-08 15:54:42 +0200

    inserting mail form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,43 +1,201 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { FaMapMarker, FaEnvelope, FaCode } from "react-icons/fa";
 
-const About = () => {
+const Contact = () => {
+  const [values, setValues] = useState({
+    fullName: "",
+    email: "",
+    subject: "",
+    feedback: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const [valid, setValid] = useState(false);
+
+  const handleFullNameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      fullName: event.target.value,
+    }));
+  };
+
+  const handleEmailInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      email: event.target.value,
+    }));
+  };
+
+  const handleSubjectlInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      subject: event.target.value,
+    }));
+  };
+
+  const handleFeedbacklInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      feedback: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+    if (values.fullName && values.email && values.subject && values.feedback) {
+      setValid(true);
+      setSubmitted(true);
+    }
+  };
   return (
     <React.Fragment>
       <h1 className="main-heading color-heading">Hier findet Ihr uns</h1>
-      <div className="contact">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5176.656141396443!2d8.0677793!3d49.5538327!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x682f70d2122924b0!2sCasa%20Verde!5e0!3m2!1sde!2sde!4v1614000047483!5m2!1sde!2sde"
-          width="98%"
-          height="500px"
-          allowfullscreen=""
-          loading="lazy"
-          title="google casa verde"
-        ></iframe>
-      </div>
-      <div className="address">
-        <div id="logo-wrapper">
-          <h1 className="logo">
-            <span className="logo_clr1">C</span>
-            <span className="logo_clr2">A</span>
-            <span className="logo_clr3">S</span>
-            <span className="logo_clr4">A</span>
-          </h1>
+      <div className="contact-wrapper">
+        <div className="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5176.656141396443!2d8.0677793!3d49.5538327!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x682f70d2122924b0!2sCasa%20Verde!5e0!3m2!1sde!2sde!4v1614000047483!5m2!1sde!2sde"
+            width="98%"
+            height="600px"
+            allowfullscreen=""
+            loading="lazy"
+            title="google casa verde"
+          ></iframe>
         </div>
-        <h3>Die Bunte Vielfalt</h3>
-        <br></br>
-        <h4>Telefon</h4>
-        <p>06351 124181</p>
-        <h4>Email</h4>
-        <p>casa-verde@t-online.de</p>
-        <h4>Address</h4>
-        <p>
-          Hauptstraße 28A
-          <br /> 67304 Eisenberg
-          <br /> Germany
-        </p>
+        <div className="address">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div id="logo-wrapper">
+              <h1 className="logo">
+                <span className="logo_clr1">C</span>
+                <span className="logo_clr2">A</span>
+                <span className="logo_clr3">S</span>
+                <span className="logo_clr4">A</span>
+              </h1>
+            </div>
+          </Link>
+          <h3>Die Bunte Vielfalt</h3>
+          <br></br>
+          <h4>Telefon</h4>
+          <p>06351 124181</p>
+          <h4>Email</h4>
+          <p>casa-verde@t-online.de</p>
+          <h4>Address</h4>
+          <p>
+            Hauptstraße 28A
+            <br /> 67304 Eisenberg
+            <br /> Germany
+          </p>
+        </div>
+        <div></div>
+        <div className="FormInfo">
+          <form
+            target="_blank"
+            action="https://formsubmit.co/dc4185d72bb2395f901272e01e22678b"
+            method="POST"
+            onSubmit={handleSubmit}
+          >
+            <div className="PartOne">
+              <span>
+                <label htmlFor="fullName">Vor- und Zuname *</label>
+                <br />
+                <input
+                  type="text"
+                  id="fullName"
+                  required
+                  placeholder="Vor- und Zuname"
+                  name="fullName"
+                  value={values.fullName}
+                  onChange={handleFullNameInputChange}
+                />
+                {submitted && !values.fullName && (
+                  <span id="full-name-error">
+                    Bitte geben Sie Ihren ganzen Namen ein!
+                  </span>
+                )}
+              </span>
+
+              <span>
+                <label htmlFor="email"> E-Mail *</label> <br />
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
+                  placeholder="Bitte E-Mail eingeben"
+                  value={values.email}
+                  onChange={handleEmailInputChange}
+                />
+                {submitted && !values.email && (
+                  <span id="email-error">
+                    Bitte geben Sie eine gültige E-Mail ein
+                  </span>
+                )}
+              </span>
+            </div>
+
+            <div className="PartTwo">
+              <label htmlFor="subject">Betreff *</label>
+              <br />
+
+              <input
+                type="text"
+                id="subject"
+                placeholder="Betreff"
+                name="subject"
+                required
+                value={values.subject}
+                onChange={handleSubjectlInputChange}
+              />
+              {submitted && !values.subject && (
+                <span id="subject-error">Bitte geben Sie einen Betreff an</span>
+              )}
+            </div>
+
+            <div className="PartThree">
+              <label htmlFor="feedback">Ihre Nachricht: *</label>
+              <br />
+              <textarea
+                name="feedback"
+                id="feedback"
+                cols="30"
+                rows="10"
+                required
+                placeholder="Hallo!"
+                value={values.feedback}
+                onChange={handleFeedbacklInputChange}
+              ></textarea>
+              {submitted && !values.feedback && (
+                <span id="feedback-error">
+                  Bitte schreiben Sie Ihre Nachricht!
+                </span>
+              )}
+            </div>
+            <div className="PartFour">
+              <button type="submit" value="Submit" target="_blank">
+                Submit
+              </button>
+            </div>
+            <input
+              type="hidden"
+              name="_autoresponse"
+              value="Thank you! your message has been received and I will reply to you as soon as possible."
+            />
+            <input type="hidden" name="_subject" value="Message received" />
+          </form>
+          {submitted && (
+            <div className="SuccessMessage">
+              Vielen Dank für Ihre E-Mail, wir melden uns bei Ihnen.
+            </div>
+          )}
+        </div>
       </div>
     </React.Fragment>
   );
 };
 
-export default About;
+export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,39 @@ const Home = () => {
     <div className="home-wrapper">
       <h1 className="main-heading color-heading">CASA Website</h1>
       <div className="home">
-        <h1>Hallo Ihr Lieben!</h1>
+        <h1>CASA - Das „grüne Haus“ wird bunt!</h1>
         <br />
-        <h2>Hier entsteht die Internetpräsenz vom</h2>
+        <h2>Neueröffnung am 03.+04. September</h2>
+        <p></p>
+        Maria Dardis-Ulbrich mit ihrem Casa Verde ist seit 2001 weithin bekannt
+        für außergewöhnliche Blumengestaltung. Nach einem Jahr Pause eröffnet
+        sie mit ihrem Partner Sven Wacker das CASA mit neuem Konzept, um Raum
+        für Kunst und Kreatives, Workshops und Vorträge, Tanz und Bewegung sowie
+        Wissenschaft und Technik zu schaffen. Die Begegnung mit der Pilates- und
+        Gymnastiklehrerin Birgit Hemmer trägt dazu bei, dass beide Frauen nun
+        ihre Träume erfüllen können. Zusammen mit Sven Wacker bilden sie das
+        Initiativteam und gestalten das neue CASA bis September um. Der
+        ehemalige Verkaufsraum wird nun zu einer freien Fläche mit toller
+        Atmosphäre. Hier bietet Birgit Hemmer Unterricht in Ballett, Pilates und
+        Barre Workout an. Kinder, Jugendliche und Erwachsene können in
+        verschiedenen Altersgruppen jede Woche trainieren und tanzen. Besonders
+        erwachsene Einsteigerinnen, Ältere, aber auch Fortgeschrittene dürfen
+        sich trauen, das neue tänzerische Training an der Stange und am Boden
+        auszuprobieren. Dafür wurde das Haus eigens mit Schwingboden,
+        Spiegelwand und Ballettstangen ausgestattet. Der Ingenieur und
+        Entwickler Sven Wacker kümmert sich im neuen Casa um die Technik sowie
+        die Gestaltung der Webseite. Er unterstützt Maria und ist offen für
+        weitere Themen rund um Gesundheit, Natur, Wissenschaft, Technik und das
+        Handwerk. Mit bunter Vielfalt öffnet das CASA auch für Gäste mit neuen
+        Ideen: externe Interessenten sind eingeladen, die Räumlichkeiten sowie
+        den Außenbereich zu mieten und das CASA als Plattform zu nutzen. Zum
+        Beispiel könnte draußen ein regionaler Obststand, ein
+        Kunsthandwerkermarkt, im Innenbereich eine Kunstausstellung oder eine
+        kleine Hochzeitsmesse stattfinden. In der CASA-Kreativwerkstatt wird
+        sich Maria Dardis-Ulbrich weiterhin um Bestellungen kümmern, ausgewählte
+        Blumenkreationen, eine nochmals erweiterte Blumenstation und
+        Blumenworkshops anbieten. Das neue bunte CASA soll neben der Blumenkunst
+        ein Forum sein für Gestaltung, Bewegung und Begegnung.
         <br />
         <div id="logo-wrapper">
           <h1 className="logo">
@@ -15,7 +45,6 @@ const Home = () => {
             <span className="logo_clr4">A</span>
           </h1>
         </div>
-
         <img
           className="casa-image"
           src={`${process.env.PUBLIC_URL}/casa.jpg`}
